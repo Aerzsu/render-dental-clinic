90d3f55001eadc8353e4361ab57a51483f105d04
eurt <sacyaten.216078@fairview.sti.edu.ph>
2025-10-11 13:05:33 +0800

    changed email port to work with render

--- dental_clinic_project/settings.py
+++ dental_clinic_project/settings.py
@@ -163,7 +163,8 @@ if not DEBUG:
 # Email Configuration (using Brevo/Sendinblue)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 587
+# EMAIL_PORT = 587 NOT WORKING FOR RENDER
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
